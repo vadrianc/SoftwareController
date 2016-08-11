0f01eac395524747b0475d4385b8bf9e1dcebfc4
Adrian Constantin Vaideanu <vaideanu.adrian@gmail.com>
2016-08-11 23:31:42 +0300

    Add interface for defining a "Rule"

---
+++ SoftwareControllerApi/Rule/IRule.cs
@@ -0,0 +1,32 @@
+﻿namespace SoftwareControllerApi.Rule
+{
+    using Action;
+
+    /// <summary>
+    /// Interface for defining a rule.
+    /// </summary>
+    interface IRule
+    {
+        /// <summary>
+        /// The name of the rule.
+        /// </summary>
+        string Name { get; }
+
+        /// <summary>
+        /// Apply the rule's actions.
+        /// </summary>
+        void Apply();
+
+        /// <summary>
+        /// Add action to be executed as part of the rule.
+        /// </summary>
+        /// <param name="action">Action to be added.</param>
+        void AddAction(IAction action);
+
+        /// <summary>
+        /// Remove action from the rule.
+        /// </summary>
+        /// <param name="action">Action to be removed.</param>
+        void RemoveAction(IAction action);
+    }
+}
