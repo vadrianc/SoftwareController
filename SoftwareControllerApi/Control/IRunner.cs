8bf649e0773b23937a97e92c3c1bc2311bf430e2
Adrian Constantin Vaideanu <vaideanu.adrian@gmail.com>
2016-08-11 23:44:54 +0300

    Add "Control" specific interfaces.

---
+++ SoftwareControllerApi/Control/IRunner.cs
@@ -0,0 +1,13 @@
+﻿namespace SoftwareControllerApi.Control
+{
+    /// <summary>
+    /// Interface for defining a runner responsible with executing the logic of a session.
+    /// </summary>
+    interface IRunner
+    {
+        /// <summary>
+        /// Execute the logic of a session.
+        /// </summary>
+        void Execute();
+    }
+}
---
+++ SoftwareControllerApi/Control/ISession.cs
@@ -0,0 +1,32 @@
+﻿namespace SoftwareControllerApi.Control
+{
+    using Rule;
+
+    /// <summary>
+    /// Interface for defining a session.
+    /// </summary>
+    interface ISession
+    {
+        /// <summary>
+        /// The name of the session.
+        /// </summary>
+        string Name { get; }
+
+        /// <summary>
+        /// Run the session.
+        /// </summary>
+        void Run();
+
+        /// <summary>
+        /// Add a new rule to the session.
+        /// </summary>
+        /// <param name="rule">The rule to be added.</param>
+        void AddRule(IRule rule);
+
+        /// <summary>
+        /// Remove the given rule.
+        /// </summary>
+        /// <param name="rule">The rule to be removed.</param>
+        void RemoveRule(IRule rule);
+    }
+}
