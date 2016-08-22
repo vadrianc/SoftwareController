1df63b82a99be895c14be8dead68224fdf96c3e0
Adrian Constantin Vaideanu <vaideanu.adrian@gmail.com>
2016-08-22 23:25:25 +0300

    Implement the ParallelAction class and add some unit tests for it.

---
+++ SoftwareControllerLib/Action/ParallelAction.cs
@@ -0,0 +1,72 @@
+﻿namespace SoftwareControllerLib.Action
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Threading.Tasks;
+    using SoftwareControllerApi.Action;
+
+    /// <summary>
+    /// Composite action that executes a collection of actions in parallel.
+    /// </summary>
+    public class ParallelAction : IParallelAction
+    {
+        private IList<IAction> mActions;
+
+        /// <summary>
+        /// Get or set the name of the action.
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ParallelAction"/> class with the given name.
+        /// </summary>
+        /// <param name="name">The name of the action.</param>
+        public ParallelAction(string name)
+        {
+            if (name == null) throw new ArgumentNullException("name", "Cannot be null.");
+            if (string.IsNullOrWhiteSpace(name)) throw new ArgumentException("Cannot be empty", "name");
+
+            Name = name;
+            mActions = new List<IAction>();
+        }
+
+        /// <summary>
+        /// Execute a collection of actions in parallel.
+        /// </summary>
+        public void Execute()
+        {
+            if (mActions.Count == 0) return;
+
+            Task[] tasks = new Task[mActions.Count];
+            int index = 0;
+
+            foreach (IAction action in mActions) {
+                Task task = new Task(() => { action.Execute(); });
+                task.Start();
+                tasks[index++] = task;
+            }
+
+            Task.WaitAll(tasks);
+        }
+
+        /// <summary>
+        /// Add a new action.
+        /// </summary>
+        /// <param name="action">The action to be added.</param>
+        public void AddAction(IAction action)
+        {
+            if (action == null) throw new ArgumentNullException("action", "Cannot be null");
+
+            mActions.Add(action);
+        }
+
+        /// <summary>
+        /// Remove an action.
+        /// </summary>
+        /// <param name="action">The action to be removed.</param>
+        public void RemoveAction(IAction action)
+        {
+            mActions.Remove(action);
+        }
+    }
+}
---
+++ SoftwareControllerLibTest/ParallelActionTest.cs
@@ -0,0 +1,39 @@
+﻿namespace SoftwareControllerLibTest
+{
+    using System;
+    using NUnit.Framework;
+    using SoftwareControllerLib.Action;
+
+    [TestFixture]
+    public class ParallelActionTest
+    {
+        [Test]
+        [TestCase("", TestName = "EmptyParallelActionName")]
+        [TestCase("   ", TestName = "WhitespaceParallelActionName")]
+        [ExpectedException(typeof(ArgumentException))]
+        [Category("ParallelAction")]
+        public void InvalidName(string name)
+        {
+            new ParallelAction(name);
+        }
+
+        [Test]
+        [ExpectedException(typeof(ArgumentNullException))]
+        [Category("ParallelAction")]
+        public void NullParallelActionName()
+        {
+            new ParallelAction(null);
+        }
+
+        [Test]
+        [Category("ParallelAction")]
+        public void CheckParallelActionName()
+        {
+            ParallelAction pAction = new ParallelAction("Test");
+            Assert.That(pAction.Name, Is.EqualTo("Test"));
+
+            pAction.Name = "NewName";
+            Assert.That(pAction.Name, Is.EqualTo("NewName"));
+        }
+    }
+}
