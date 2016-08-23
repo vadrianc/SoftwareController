2f4aa1f81a0590ef3bd661f8f98128f35c2f37b9
Adrian Constantin Vaideanu <vaideanu.adrian@gmail.com>
2016-08-23 22:05:30 +0300

    Implement the Session class and add unit tests for it.

---
+++ SoftwareControllerLib/Control/Session.cs
@@ -0,0 +1,67 @@
+﻿namespace SoftwareControllerLib.Control
+{
+    using System;
+    using System.Collections.Generic;
+    using SoftwareControllerApi.Control;
+    using SoftwareControllerApi.Rule;
+
+    /// <summary>
+    /// Software controller session responsible with applying a set of rules.
+    /// </summary>
+    public class Session : ISession
+    {
+        private IList<IRule> mRules;
+
+        /// <summary>
+        /// Initialize a new instance of the <see cref="Session"/> class with the given name.
+        /// </summary>
+        /// <param name="name">The name of the session.</param>
+        public Session(string name)
+        {
+            if (name == null) throw new ArgumentNullException("name", "Cannot be null");
+            if (string.IsNullOrWhiteSpace(name)) throw new ArgumentException("Cannot be empty", "name");
+
+            Name = name;
+            mRules = new List<IRule>();
+        }
+
+        /// <summary>
+        /// Get or set the name of the session.
+        /// </summary>
+        public string Name { get; set;}
+
+        /// <summary>
+        /// Applies a set of rules in a sequential order.
+        /// </summary>
+        public void Run()
+        {
+            if (mRules.Count == 0) return;
+
+            foreach (IRule rule in mRules) {
+                rule.Apply();
+            }
+        }
+
+        /// <summary>
+        /// Add a new rule.
+        /// </summary>
+        /// <param name="rule">The rule to be added.</param>
+        public void AddRule(IRule rule)
+        {
+            if (rule == null) throw new ArgumentNullException("rule", "Cannot be null");
+
+            mRules.Add(rule);
+        }
+
+        /// <summary>
+        /// Remove a rule.
+        /// </summary>
+        /// <param name="rule">The rule to be removed.</param>
+        public void RemoveRule(IRule rule)
+        {
+            if (rule == null) throw new ArgumentNullException("rule", "Cannot be null");
+
+            mRules.Remove(rule);
+        }
+    }
+}
---
+++ SoftwareControllerLibTest/SessionTest.cs
@@ -0,0 +1,57 @@
+﻿namespace SoftwareControllerLibTest
+{
+    using System;
+    using NUnit.Framework;
+    using SoftwareControllerLib.Control;
+
+    [TestFixture]
+    public class SessionTest
+    {
+        [Test]
+        [TestCase("", TestName = "EmptySessionName")]
+        [TestCase("   ", TestName = "WhitespaceSessionName")]
+        [ExpectedException(typeof(ArgumentException))]
+        [Category("Session")]
+        public void InvalidName(string name)
+        {
+            new Session(name);
+        }
+
+        [Test]
+        [ExpectedException(typeof(ArgumentNullException))]
+        [Category("Session")]
+        public void NullSessionName()
+        {
+            new Session(null);
+        }
+
+        [Test]
+        [Category("Session")]
+        public void CheckSessionName()
+        {
+            Session session = new Session("Test");
+            Assert.That(session.Name, Is.EqualTo("Test"));
+
+            session.Name = "NewName";
+            Assert.That(session.Name, Is.EqualTo("NewName"));
+        }
+
+        [Test]
+        [ExpectedException(typeof(ArgumentNullException))]
+        [Category("Session")]
+        public void AddNullRule()
+        {
+            Session session = new Session("Test");
+            session.AddRule(null);
+        }
+
+        [Test]
+        [ExpectedException(typeof(ArgumentNullException))]
+        [Category("Session")]
+        public void RemoveNullRule()
+        {
+            Session session = new Session("Test");
+            session.RemoveRule(null);
+        }
+    }
+}
