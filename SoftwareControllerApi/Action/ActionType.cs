a8f18cab614627a4630cedd80396fe3ae88cf156
Adrian Constantin Vaideanu <vaideanu.adrian@gmail.com>
2016-09-20 21:30:53 +0300

    Add enumeration containing action types and an abstract action factory.

---
+++ SoftwareControllerApi/Action/ActionType.cs
@@ -0,0 +1,33 @@
+﻿namespace SoftwareControllerApi.Action
+{
+    /// <summary>
+    /// Enumeration containing types of actions.
+    /// </summary>
+    public enum ActionType
+    {
+        /// <summary>
+        /// Click action.
+        /// </summary>
+        CLICK,
+
+        /// <summary>
+        /// Collect action.
+        /// </summary>
+        COLLECT,
+
+        /// <summary>
+        /// Navigate action.
+        /// </summary>
+        NAVIGATE,
+
+        /// <summary>
+        /// Type action.
+        /// </summary>
+        TYPE,
+
+        /// <summary>
+        /// Wait action.
+        /// </summary>
+        WAIT
+    }
+}
---
+++ SoftwareControllerLib/Action/ActionFactoryBase.cs
@@ -0,0 +1,17 @@
+﻿using SoftwareControllerApi.Action;
+
+namespace SoftwareControllerLib.Action
+{
+    /// <summary>
+    /// Base class for creating an action factory.
+    /// </summary>
+    public abstract class ActionFactoryBase
+    {
+        /// <summary>
+        /// Create a new action given the type.
+        /// </summary>
+        /// <param name="type">The type of the action that is to be created.</param>
+        /// <returns>The action.</returns>
+        public abstract IAction Create(ActionType type);
+    }
+}
