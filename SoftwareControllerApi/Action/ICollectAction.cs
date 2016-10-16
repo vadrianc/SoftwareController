e887853be5ef8263ac48948cac5109d10d23e29b
Adrian Constantin Vaideanu <vaideanu.adrian@gmail.com>
2016-10-16 21:31:02 +0300

    Remove Regex from the ICollectAction interface, since it's too specific.

--- SoftwareControllerApi/Action/ICollectAction.cs
+++ SoftwareControllerApi/Action/ICollectAction.cs
@@ -1,15 +1,7 @@
 ﻿namespace SoftwareControllerApi.Action
 {
-    using System.Text.RegularExpressions;
-
     /// <summary>
     /// Interface for defining an action related to data collection.
     /// </summary>
-    public interface ICollectAction : ISequentialAction
-    {
-        /// <summary>
-        /// Get the regular expression used to identify an item to collect from.
-        /// </summary>
-        Regex RegEx { get; }
-    }
+    public interface ICollectAction : ISequentialAction { }
 }
