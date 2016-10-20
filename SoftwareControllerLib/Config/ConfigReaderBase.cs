ba9987212465ea28bee4610b404496b9162eba50
Adrian Constantin Vaideanu <vaideanu.adrian@gmail.com>
2016-10-21 01:08:13 +0300

    Rename ConfigReader to ConfigReaderBase, make ConfigReaderBase generic and rewrite automaton rule parsing logic.

--- SoftwareControllerLib/Config/ConfigReaderBase.cs
+++ SoftwareControllerLib/Config/ConfigReaderBase.cs
@@ -13,41 +13,33 @@ namespace SoftwareControllerLib.Config
     /// rules. Any class that derives from this class shall implement the custom logic for parsing 
     /// specific actions and settings defined within the configuration file.
     /// </remarks>
-    public abstract class ConfigReaderBase
+    public abstract class ConfigReaderBase<T> where T : Session
     {
         protected XmlReader m_Reader;
+        protected T m_Session;
 
         /// <summary>
-        /// Initializes a new instance of the <see cref="ConfigReaderBase"/> class with the given session and configuration file path.
+        /// Initializes a new instance of the <see cref="ConfigReaderBase"/> class with the given configuration file path.
         /// </summary>
-        /// <param name="session">The session.</param>
         /// <param name="config">The configuration file path.</param>
-        public ConfigReaderBase(Session session, string config)
+        public ConfigReaderBase(string config)
         {
-            if (session == null) throw new ArgumentNullException("session", "Cannot be null");
             if (config == null) throw new ArgumentNullException("config", "Cannot be null");
             if (string.IsNullOrWhiteSpace(config)) throw new ArgumentException("Cannot be empty or whitespace only", "config");
 
             m_Reader = XmlReader.Create(config);
-            Session = session;
-        }
-
-        /// <summary>
-        /// Get or set the session.
-        /// </summary>
-        public Session Session
-        {
-            get;
-            private set;
         }
 
         /// <summary>
         /// Read the configuration file and update the session.
         /// </summary>
-        public void Read()
+        /// <returns>The session containing the parsed configuration settings.</returns>
+        public T Read()
         {
+            InitSession();
             ReadAutomaton();
-            ReadSettings();
+
+            return m_Session;
         }
 
         private void ReadAutomaton()
@@ -56,30 +48,28 @@ namespace SoftwareControllerLib.Config
                 throw new ArgumentException("No <automaton> element found");
             }
 
-            if (m_Reader.ReadToFollowing("automaton")) {
-                throw new ArgumentException("Cannot define more than one <automaton> elements");
-            }
+            XmlReader automatonReader = m_Reader.ReadSubtree();
 
-            while (m_Reader.ReadToDescendant("rule")) {
-                Rule rule = new Rule(m_Reader.GetAttribute("name"));
-
-                while (m_Reader.ReadToDescendant("action")) {
-                    ReadActions(rule);
+            while (automatonReader.Read()) {
+                if (automatonReader.NodeType == XmlNodeType.Element && automatonReader.Name.Equals("rule")) {
+                    Rule rule = new Rule(automatonReader.GetAttribute("name"));
+                    m_Session.AddRule(rule);
+                    XmlReader ruleReader = automatonReader.ReadSubtree();
+                    ReadActions(ruleReader, rule);
                 }
-
-                Session.AddRule(rule);
             }
         }
 
         /// <summary>
         /// Read the application specific settings from the configuration file.
         /// </summary>
-        protected abstract void ReadSettings();
+        protected abstract void InitSession();
 
         /// <summary>
         /// Read the application specific actions from the configuration file.
         /// </summary>
+        /// <param name="ruleReader">The XML representing the rule.</param>
         /// <param name="rule">The rule where the read actions are to be added.</param>
-        protected abstract void ReadActions(IRule rule);
+        protected abstract void ReadActions(XmlReader ruleReader, IRule rule);
     }
 }
