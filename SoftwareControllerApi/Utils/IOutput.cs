9634969697c3520d77214bb80d849a083e687840
Adrian Constantin Vaideanu <vaideanu.adrian@gmail.com>
2016-10-25 23:14:23 +0300

    Add interface for defining common output functionality. Implemented concrete class for outputting to the console.

---
+++ SoftwareControllerApi/Utils/IOutput.cs
@@ -0,0 +1,26 @@
+﻿namespace SoftwareControllerApi.Utils
+{
+    /// <summary>
+    /// Interface for defining an output class.
+    /// </summary>
+    public interface IOutput
+    {
+        /// <summary>
+        /// Display normal message.
+        /// </summary>
+        /// <param name="msg">The normal message to be displayed.</param>
+        void Message(string msg);
+
+        /// <summary>
+        /// Display warning message.
+        /// </summary>
+        /// <param name="msg">The warning message to be displayed.</param>
+        void Warning(string msg);
+
+        /// <summary>
+        /// Display error message.
+        /// </summary>
+        /// <param name="msg">The error message to be displayed.</param>
+        void Error(string msg);
+    }
+}
---
+++ SoftwareControllerLib/Utils/ConsoleOutput.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using SoftwareControllerApi.Utils;
+
+namespace SoftwareControllerLib.Utils
+{
+    /// <summary>
+    /// Class for outputting to a console.
+    /// </summary>
+    public class ConsoleOutput : IOutput
+    {
+        private static ConsoleOutput m_Instance;
+
+        /// <summary>
+        /// Get the singleton instance.
+        /// </summary>
+        public static ConsoleOutput Instance
+        {
+            get
+            {
+                if (m_Instance == null) {
+                    m_Instance = new ConsoleOutput();
+                }
+
+                return m_Instance;
+            }
+        }
+
+        /// <summary>
+        /// Display error message.
+        /// </summary>
+        /// <param name="msg">The error message to be displayed.</param>
+        public void Error(string msg)
+        {
+            Console.Out.WriteLine(string.Format("ERROR: {0}", msg));
+        }
+
+        /// <summary>
+        /// Display normal message.
+        /// </summary>
+        /// <param name="msg">The normal message to be displayed.</param>
+        public void Message(string msg)
+        {
+            Console.Out.WriteLine(msg);
+        }
+
+        /// <summary>
+        /// Display warning message.
+        /// </summary>
+        /// <param name="msg">The warning message to be displayed.</param>
+        public void Warning(string msg)
+        {
+            Console.Out.WriteLine(string.Format("WARNING: {0}", msg));
+        }
+    }
+}
