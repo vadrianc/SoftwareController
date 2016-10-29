cfeddec4f5fe790b50fc5b1dee66ec32783bf83f
Adrian Constantin Vaideanu <vaideanu.adrian@gmail.com>
2016-10-29 11:44:43 +0300

    Adding unit tests for the ConfigReaderBase class.

---
+++ SoftwareControllerLibTest/Config/ConfigReaderBaseTest.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using NUnit.Framework;
+using SoftwareControllerLib.Control;
+
+namespace SoftwareControllerLibTest.Config
+{
+    [TestFixture]
+    public class ConfigReaderBaseTest
+    {
+        [Test]
+        [ExpectedException(typeof(ArgumentNullException))]
+        [Category("ConfigReaderBase")]
+        public void NullConfig()
+        {
+            new DummyConfigReader(null);
+        }
+
+        [Test]
+        [ExpectedException(typeof(ArgumentException))]
+        [Category("ConfigReaderBase")]
+        public void EmptyConfig()
+        {
+            new DummyConfigReader(string.Empty);
+        }
+
+        [Test]
+        [ExpectedException(typeof(ArgumentException))]
+        [Category("ConfigReaderBase")]
+        public void WhitespaceOnlyConfig()
+        {
+            new DummyConfigReader("   ");
+        }
+
+        [Test]
+        [Category("ConfigReaderBase")]
+        public void CheckImplMethodExecution()
+        {
+            DummyConfigReader reader = new DummyConfigReader("Resources\\default.xml");
+            reader.Read();
+
+            Assert.That(reader.ReadActionsDone, Is.True);
+            Assert.That(reader.ReadInitSessionDone, Is.True);
+        }
+
+        [Test]
+        [Category("ConfigReaderBase")]
+        public void RuleCount()
+        {
+            DummyConfigReader reader = new DummyConfigReader("Resources\\default.xml");
+            Session session = reader.Read();
+
+            Assert.That(session, Is.Not.Null);
+            Assert.That(session.Rules.Count, Is.EqualTo(2));
+        }
+
+        [Test]
+        [ExpectedException(typeof(ArgumentException), ExpectedMessage = "No <automaton> element found")]
+        [Category("ConfigReaderBase")]
+        public void MissingAutomatonTag()
+        {
+            DummyConfigReader reader = new DummyConfigReader("Resources\\missing_automaton.xml");
+            reader.Read();
+        }
+
+        [Test]
+        [Category("ConfigReaderBase")]
+        public void NoRules()
+        {
+            DummyConfigReader reader = new DummyConfigReader("Resources\\missing_rules.xml");
+            Session session = reader.Read();
+
+            Assert.That(session, Is.Not.Null);
+            Assert.That(reader.ReadActionsDone, Is.False);
+            Assert.That(reader.ReadInitSessionDone, Is.True);
+            Assert.That(session.Rules.Count, Is.EqualTo(0));
+        }
+    }
+}
---
+++ SoftwareControllerLibTest/Config/DummyConfigReader.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Threading;
+using System.Xml;
+using SoftwareControllerApi.Rule;
+using SoftwareControllerLib.Config;
+using SoftwareControllerLib.Control;
+
+namespace SoftwareControllerLibTest.Config
+{
+    /// <summary>
+    /// Dummy class used only for testing the <see cref="ConfigReaderBase{T}"/> logic.
+    /// </summary>
+    public class DummyConfigReader : ConfigReaderBase<Session>
+    {
+        public bool ReadInitSessionDone { get; private set; }
+        public bool ReadActionsDone { get; private set; }
+
+        public DummyConfigReader(string config) : base(config) { }
+
+        protected override void InitSession()
+        {
+            m_Session = new Session("dummy");
+            ReadInitSessionDone = true;
+        }
+
+        protected override void ReadActions(XmlReader ruleReader, IRule rule)
+        {
+            ReadActionsDone = true;
+        }
+    }
+}
